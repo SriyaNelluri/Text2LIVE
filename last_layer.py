d8fb57c64697b4562a7f0586cb6094e4af6c8a32
SN15 <60146450+SriyaNelluri@users.noreply.github.com>
2023-05-12 12:16:40 +0530

    Update last_layer.py

--- last_layer.py
+++ last_layer.py
@@ -1,1 +1,27 @@
-
+import torch
+import numpy as np
+import torchvision.transforms as T
+from models.clip_relevancy import ClipRelevancy
+from util.util import get_screen_termplate, get_text_criterion, get_augmentations_template
+class Last_Layer(torch.nn.Module):
+  def __init__(self, cfg, clip_extractor):
+    self.cfg = cfg
+    template = get_augmentations_template()
+    self.target_comp_e = clip_extractor.get_text_embedding(cfg["comp_text"], template)
+    self.clip_extractor = clip_extractor
+    self.text_criterion = get_text_criterion(cfg)
+  
+  def forward(self,inputs,outputs):
+      for img in outputs:  # avoid memory limitations
+          img_e = self.clip_extractor.get_image_embedding(img.unsqueeze(0))
+         
+  def func(self,outputs):
+    model=nn.Sequential(
+            nn.ReLU()
+    )
+    
+      
+  
+      
+     
+  
